f06f87f68f64c9545b00d984374c61caa801a9d0
dafeng <eric@163.com>
2023-04-16 22:13:46 +0800

    branch master commit

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,14 @@
+package service;
+
+/**
+ * @author xiangfeng
+ * @create 2023-04 21:57
+ */
+public interface UserService {
+
+    void addUser(String username);
+
+    void deleteUser(String  username,String password);
+
+    void updateUser(String username, String password);
+}
---
+++ src/main/java/service/impl/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package service.impl;
+
+import service.UserService;
+
+/**
+ * @author xiangfeng
+ * @create 2023-04 21:59
+ */
+public class UserServiceImpl implements UserService {
+    @Override
+    public void addUser(String username) {
+
+    }
+
+    @Override
+    public void deleteUser(String username,String password) {
+
+    }
+
+    @Override
+    public void updateUser(String username, String password) {
+
+    }
+}
